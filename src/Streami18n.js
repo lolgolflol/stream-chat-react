eba4525bf636aa6e04339bee85fc7839917c550d
Watcharapon Sarnsanit <watcharapon@computerlogy.com>
2020-07-27 23:38:45 +0700

    date

--- src/Streami18n.js
+++ src/Streami18n.js
@@ -35,6 +35,17 @@ import 'dayjs/locale/en';
 
 Dayjs.extend(updateLocale);
 
+Dayjs.updateLocale('th', {
+  calendar: {
+    sameDay: '[วันนี้] LT',
+    nextDay: '[พรุ่งนี้] LT',
+    nextWeek: 'dddd [ที่] LT',
+    lastDay: '[เมื่อวานนี้] LT',
+    lastWeek: '[ล่าสุด] dddd [สัปดาห์] LT',
+    sameElse: 'L',
+  },
+});
+
 Dayjs.updateLocale('nl', {
   calendar: {
     sameDay: '[vandaag om] LT',
