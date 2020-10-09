446a4dd5c5c1795a714f2eda3fa6c0973657f6a9
Watcharapon Sarnsanit <watcharapon@computerlogy.com>
2020-10-10 02:47:01 +0700

    message sime

--- src/components/Message/MessageSimple.js
+++ src/components/Message/MessageSimple.js
@@ -622,11 +622,11 @@ class MessageSimple extends PureComponent {
             <div
               className={`str-chat__message-data str-chat__message-simple-data`}
             >
-              !this.isMine() ? (
-              <span className="str-chat__message-simple-name">
-                {message.user.name || message.user.id}
-              </span>
-              ) : null
+              {!this.isMine() ? (
+                <span className="str-chat__message-simple-name">
+                  {message.user.name || message.user.id}
+                </span>
+              ) : null}
               <span className="str-chat__message-simple-timestamp">{when}</span>
             </div>
           </div>
